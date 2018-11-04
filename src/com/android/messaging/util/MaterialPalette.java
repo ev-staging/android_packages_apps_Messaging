856eaaef2b3eb557ce6c110d0e95c99ae1726640
Nick Reuter <nreuter85@gmail.com>
2018-11-04 01:34:49 -0600

    Messaging: generate background color of letter tiles
    Change-Id: Ib1e4c3b4ba2ffe0a2ab830bf0dbe58afdf7dbf6b

--- src/com/android/messaging/util/MaterialPalette.java
+++ src/com/android/messaging/util/MaterialPalette.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2014 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,15 +13,84 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.android.messaging.util;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class MaterialPalette{
-    public final int mPrimaryColor;
-    public final int mSecondaryColor;
+public class MaterialPalette implements Parcelable {
+    private final int mPrimaryColor;
+    private final int mSecondaryColor;
 
-    public MaterialPalette(final int primaryColor, final int secondaryColor) {
+    public MaterialPalette(int primaryColor, int secondaryColor) {
         mPrimaryColor = primaryColor;
         mSecondaryColor = secondaryColor;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        MaterialPalette other = (MaterialPalette) obj;
+        if (mPrimaryColor != other.mPrimaryColor) {
+            return false;
+        }
+        if (mSecondaryColor != other.mSecondaryColor) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + mPrimaryColor;
+        result = prime * result + mSecondaryColor;
+        return result;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(mPrimaryColor);
+        dest.writeInt(mSecondaryColor);
+    }
+
+    private MaterialPalette(Parcel in) {
+        mPrimaryColor = in.readInt();
+        mSecondaryColor = in.readInt();
+    }
+
+    public static final Creator<MaterialPalette> CREATOR = new Creator<MaterialPalette>() {
+            @Override
+            public MaterialPalette createFromParcel(Parcel in) {
+                return new MaterialPalette(in);
+            }
+
+            @Override
+            public MaterialPalette[] newArray(int size) {
+                return new MaterialPalette[size];
+            }
+    };
+
+    public int getPrimary() {
+        return mPrimaryColor;
+    }
+
+    public int getSecondary() {
+        return mSecondaryColor;
     }
 }
